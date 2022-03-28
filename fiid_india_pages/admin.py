2e5ec5302808a39696f2c55936dfee1646e9e070
Joise MP <joise1124@gmail.com>
2022-03-28 14:58:26 +0530

    Created Image model Nested it inside Page model and added search funtinality to Carousel

--- fiid_india_pages/admin.py
+++ fiid_india_pages/admin.py
@@ -1,5 +1,8 @@
+from operator import mod
+from pyexpat import model
+from re import search
 from django.contrib import admin
-from .models import Page, Summary, Paragraph, Carousel
+from .models import Page, Summary, Paragraph, Carousel, Image
 
 class SummaryTabularInline(admin.TabularInline):
     model = Summary
@@ -7,13 +10,20 @@ class SummaryTabularInline(admin.TabularInline):
 class ParagraphTabularInline(admin.TabularInline):
     model = Paragraph
 
+class ImageTabularInline(admin.TabularInline):
+    model = Image
+
 class PageAdmin(admin.ModelAdmin):
     list_display = ('name',)
     search_fields=('name',)
-    inlines = [SummaryTabularInline, ParagraphTabularInline]
+    inlines = [SummaryTabularInline, ParagraphTabularInline, ImageTabularInline]
 
     class Meta:
         model = Page
 
+class CarouselAdmin(admin.ModelAdmin):
+    list_display = ('heading',)
+    search_fields = ('heading',)
+
 admin.site.register(Page, PageAdmin)
-admin.site.register(Carousel)
+admin.site.register(Carousel, CarouselAdmin)
--- fiid_india_pages/models.py
+++ fiid_india_pages/models.py
@@ -1,3 +1,4 @@
+from distutils.command.upload import upload
 from django.db import models
 from django.dispatch import receiver
 from django.db.models.signals import pre_delete
@@ -5,7 +6,7 @@ from django.db.models.signals import pre_delete
 class Carousel(models.Model):
     heading = models.CharField(max_length=300)
     text = models.CharField(max_length=600)
-    image = models.ImageField(upload_to='images/')
+    image = models.ImageField(upload_to='images/carousel/')
 
     def __str__(self):
         return(self.heading)
@@ -15,6 +16,13 @@ class Page(models.Model):
 
     def __str__(self):
         return str(self.name)
+
+class Image(models.Model):
+    page = models.ForeignKey(Page, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='images/pages/')
+
+    def __str__(self):
+        return str(self.page)
 
 class Summary(models.Model):
     page = models.OneToOneField(Page, on_delete=models.CASCADE)
@@ -32,5 +40,11 @@ class Paragraph(models.Model):
 
 @receiver(pre_delete, sender=Carousel)
 def Carousel_delete(sender, instance, **kwargs):
+    # Pass false so FileField doesn't save the model.
+    instance.image.delete(False)
+
+
+@receiver(pre_delete, sender=Image)
+def Image_delete(sender, instance, **kwargs):
     # Pass false so FileField doesn't save the model.
     instance.image.delete(False)

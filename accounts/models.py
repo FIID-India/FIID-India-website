6c3eb8cdb4888622a889ce4d3398fc9c8839ae0f
Joise MP <joise1124@gmail.com>
2022-03-28 09:19:50 +0530

    added field to user model

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,7 @@
+from operator import mod
+from pyexpat import model
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    pass
+    is_admin = models.BooleanField(default=False)

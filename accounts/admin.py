12414623d91f1347271d504f2e257a0d55526181
Joise MP <joise1124@gmail.com>
2022-03-29 20:12:18 +0530

    added filter to user model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import User
 
-admin.site.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_filter = ('is_staff', 'is_superuser')
+
+admin.site.register(User, UserAdmin)

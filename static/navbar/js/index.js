873d15ecdadd81be624a8c1fa213619c40abeaa6
Joise MP <joise1124@gmail.com>
2022-04-03 14:49:03 +0530

    made some changes added bootstrap and fontawesome files made navbar stick to the top removed scroll to top button

--- static/navbar/js/index.js
+++ static/navbar/js/index.js
@@ -1,21 +1,3 @@
-/* GO TO TOP
--------------------------------------------------- */
-var mybutton = document.getElementById("myBtn");
-
-window.onscroll = function() {scrollFunction()};
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
-
 /* SCROLL OR REDIRECT ON CLICK
 -------------------------------------------------- */
 let ScrollToSection = (id, url) =>{

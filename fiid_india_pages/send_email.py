7a05352fdf844d96bdb27c67f746b3c6030680f0
Joise MP <joise1124@gmail.com>
2022-04-21 12:05:20 +0530

    made form submission fast

--- fiid_india_pages/send_email.py
+++ fiid_india_pages/send_email.py
@@ -1,10 +1,18 @@
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
 from django.utils.html import strip_tags
+import threading
+
+class EmailThread(threading.Thread):
+    def __init__(self, email):
+        self.email = email
+        threading.Thread.__init__(self)
+    def run(self):
+        self.email.send(fail_silently=False)
 
 
 def contact_email(name, message, email_to, email_from, email_subject, email_template):
-    html_content = render_to_string(email_template, {'name':name,'message':message, 'email':email_from})
+    html_content = render_to_string(email_template, {'name':name,'message':message, 'email':email_from, 'subject':email_subject})
     text_content = strip_tags(html_content)
     email = EmailMultiAlternatives(
         email_subject,
@@ -13,7 +21,7 @@ def contact_email(name, message, email_to, email_from, email_subject, email_temp
         email_to
     )
     email.attach_alternative(html_content, 'text/html')
-    email.send()
+    EmailThread(email).start()
 
 def subscriber_welcome_email(full_name, subject, to_email, email_template):
     html_content = render_to_string(email_template, {'full_name':full_name, 'email':to_email})
@@ -25,7 +33,7 @@ def subscriber_welcome_email(full_name, subject, to_email, email_template):
         to_email
     )
     email.attach_alternative(html_content, 'text/html')
-    email.send()
+    EmailThread(email).start()
 
 
 def subscriber_email(full_name, subject, to_email, message, email_template, file):
@@ -37,10 +45,6 @@ def subscriber_email(full_name, subject, to_email, message, email_template, file
         None,
         to_email
     )
-    try:
-        email.attach_file(file)     
-    except:
-        pass
 
     email.attach_alternative(html_content, 'text/html')
-    email.send()
+    EmailThread(email).start()

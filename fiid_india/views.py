124a24ef305af6a19a5fd52837cabc84e66ffddb
Joise MP <joise1124@gmail.com>
2023-04-04 14:40:19 +0530

    Created home page view and added templates and design elements

--- fiid_india/views.py
+++ fiid_india/views.py
@@ -1,3 +1,4 @@
-from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+class HomePageView(generic.TemplateView):
+    template_name = 'fiid_india/home.html'
